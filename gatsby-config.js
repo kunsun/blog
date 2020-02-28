3081cafb521823b6746098e1a27d876753120f60
yangkun <yangkunid91@gmail.com>
2020-02-28 18:31:47 +0800

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,14 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: path.resolve('./content/_posts'),
+      },
+    },
+    'gatsby-transformer-remark',
+  ]
 }
